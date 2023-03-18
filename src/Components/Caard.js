ec53e828700cd102ba0f4a7426d02734e34d7250
Sivaraman R <sivaraman.ec19@bitsathy.ac.in>
2023-03-18 12:25:21 +0530

    card

--- src/Components/Caard.js
+++ src/Components/Caard.js
@@ -1,6 +1,7 @@
 const Card=[
     {title:"gm",desc:"Good Morning"}
-,   {title:"gn",desc:"Good Night"}
+,   {title:"gn",desc:"Good Nights"},
+    {title:"gmn",desc:"Good mid Nights"}
 ,   {title:"ge",desc:"Good Evening"}];
 export  default Card;
 
